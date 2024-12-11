799f3b9a491716b4aad565cf0c4564a90831c899
Hui Zhang <100298240+HuiZhang95@users.noreply.github.com>
2024-12-12 00:14:00 +0100

    Update text_classification_models.py

--- items_hz/text_classification_models.py
+++ items_hz/text_classification_models.py
@@ -11,6 +11,30 @@ import pandas as pd
 # from items_hz.RNN_pipeline import RNN_pipeline
 # from SVC_pipeline import SVC_pipeline
 
+#@st.cache_data
+def load_rnn():
+    pass
+    # model = tf.keras.models.load_model('images_hz/model_RNN.keras')
+    # with open('images_hz/model_RNN_tokenizer.pkl','rb') as f:  # Python 3: open(..., 'rb')
+    #     tokenizer = pickle.load(f)
+    # return model, tokenizer
+
+#@st.cache_data
+def load_svc():
+    pass
+    # with open('images_hz/model_SVC.pkl','rb') as f:
+    #     model, _, _ = pickle.load(f)
+    # return model
+
+def prediction(classifier, user_input_word):
+    if classifier == 'RNN':
+        model, tokenizer = load_rnn()
+        rsl = 5#RNN_pipeline(model, user_input_word, tokenizer)
+    elif classifier == 'SVC':
+        model, tokenizer = load_svc()
+        rsl = 10#SVC_pipeline(model, user_input_word, tokenizer)
+    return rsl
+
 def text_classification_models():
     
     st.markdown("<h3>Customized recurrent neural network (RNN)</h3>", unsafe_allow_html = True)
@@ -60,30 +84,7 @@ def text_classification_models():
         img = Image.open("images_hz/SVM result.jpg")
         st.image(img, use_container_width = True)
 
-    
-    #@st.cache_data
-    def load_rnn():
-        pass
-        # model = tf.keras.models.load_model('images_hz/model_RNN.keras')
-        # with open('images_hz/model_RNN_tokenizer.pkl','rb') as f:  # Python 3: open(..., 'rb')
-        #     tokenizer = pickle.load(f)
-        # return model, tokenizer
-    
-    #@st.cache_data
-    def load_svc():
-        pass
-        # with open('images_hz/model_SVC.pkl','rb') as f:
-        #     model, _, _ = pickle.load(f)
-        # return model
-    
-    def prediction(classifier, user_input_word):
-        if classifier == 'RNN':
-            model, tokenizer = load_rnn()
-            rsl = 5#RNN_pipeline(model, user_input_word, tokenizer)
-        elif classifier == 'SVC':
-            model, tokenizer = load_svc()
-            rsl = 10#SVC_pipeline(model, user_input_word, tokenizer)
-        return rsl
+
 
 
     # use the st.selectbox() method to choose between the RandomForest classifier, the SVM classifier and the LogisticRegression classifier. Then return to the Streamlit web application to view the select box.
@@ -92,9 +93,9 @@ def text_classification_models():
     st.write('The chosen model is :', option)
 
     
-    user_input_word = st.text_input("Input a sentense: ", 'Merry Christmas!')
-    output_st = prediction(option, user_input_word)
-    st.write('The input text is likely to be category :\n', output_st)
+    # user_input_word = st.text_input("Input a sentense: ", 'Merry Christmas!')
+    # output_st = prediction(option, user_input_word)
+    # st.write('The input text is likely to be category :\n', output_st)
 
     
         
