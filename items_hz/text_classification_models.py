bf2e8a79f212c73c967af737a9d3ec465ac83481
Hui Zhang <100298240+HuiZhang95@users.noreply.github.com>
2024-12-11 17:18:40 +0100

    1

--- items_hz/text_classification_models.py
+++ items_hz/text_classification_models.py
@@ -1,5 +1,11 @@
 import streamlit as st
 from PIL import Image
+import pickle
+
+# from sklearn.ensemble import RandomForestClassifier
+# from sklearn.svm import SVC
+# from sklearn.linear_model import LogisticRegression
+# from sklearn.metrics import confusion_matrix
 
 def text_classification_models():
     
@@ -47,6 +53,40 @@ def text_classification_models():
         st.write("Weighted F1 score is 0.81")
         img = Image.open("images_hz/SVM result.jpg")
         st.image(img, use_container_width = True)
+
+    
+
+    @st.cache_data
+    def load_rnn():
+        with open('model_RNN.pkl','rb') as f:  # Python 3: open(..., 'rb')
+            _, model, _, _ = pickle.load(f)
+        return model
+    
+    @st.cache_data
+    def load_svc():
+        with open('images_hz/model_SVC.pkl','rb') as f:
+            model, _, _ = pickle.load(f)
+        return model
+    
+    def prediction(classifier):
+        if classifier == 'RNN':
+            clf = load_rnn()
+        elif classifier == 'SVC':
+            clf = load_svc()
+        return clf
+    
+    def scores(clf, user_input_word):
+        return clf.predict(user_input_word)
+
+
+    # use the st.selectbox() method to choose between the RandomForest classifier, the SVM classifier and the LogisticRegression classifier. Then return to the Streamlit web application to view the select box.
+    choice = ['RNN', 'SVC']
+    option = st.selectbox('Choice of the model', choice)
+    st.write('The chosen model is :', option)
+
+    user_input_word = st.text_input("Input a sentense: ", 'Merry Christmas!')
+    output_st = scores(user_input_word)
+    st.write('The input text is likely to be category :\n', output_st)
 
     
         
