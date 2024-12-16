2f8972ef1bea7b89c6a8a87d09b23263584f1388
Hui Zhang <100298240+HuiZhang95@users.noreply.github.com>
2024-12-16 01:15:08 +0100

    Update text_classification_models.py

--- items_hz/text_classification_models.py
+++ items_hz/text_classification_models.py
@@ -6,7 +6,6 @@ import pandas as pd
 
 def text_classification_models():
     
-<<<<<<< Updated upstream
     st.markdown("<h3>Customized Recurrent Neural Network (RNN)</h3>", unsafe_allow_html = True)
     with st.expander("click here for details"):
         st.markdown("<h3>Background + procedure</h3>", unsafe_allow_html = True)
@@ -90,21 +89,3 @@ def text_classification_models():
     
         
 
-=======
-    with st.expander("<h2>Customized recurrent neural network (RNN)</h2>", unsafe_allow_html = True):
-        st.markdown("<h3>Background + procedure (RNN)</h3>", unsafe_allow_html = True)
-
-        st.write("Recurrent layers are designed to capture sequential relationships within the data."
-                "This is especially important for the text information in the present project. "
-                " "
-                "Procedure: Train-test split --> tokenize sequences --> train model"
-                "Here is the achitechture of the model:")
-        
-        with st.expander("RNN achitechture"):
-            img = Image.open("images/RNN achitechture.jpg")
-            st.image(img, use_container_width = True)
-
-        st.write("For this project, we have use the three pre-trained models as the backbone "
-                "for our model architecture, ResNet50 (He et al., 2016), EfficientNetV2 "
-                "(Tan & Le, 2021), and ViT (Dosovitskiy et al., 2021).")
->>>>>>> Stashed changes
