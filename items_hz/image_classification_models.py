07195a0b76b4231a67730ee05b1007eba333fcf2
Daniel <daniel@datascientest.com>
2025-07-14 17:58:53 +0200

    Update image_classification_models.py

--- items_hz/image_classification_models.py
+++ items_hz/image_classification_models.py
@@ -180,106 +180,110 @@ def image_classification_models():
 
     st.markdown("<h2>EfficientNetV2</h2>", unsafe_allow_html = True)
 
-    st.write("EfficientNetV2 was designed to address the probelm that comes with training "
-             "increasingly large datasets. Namely, the amount of training required to "
-             "achieve a converged model systematically tends to increase as there are more "
-             "parameters and data to be trained on. To overcome these problems, EfficientNetV2 "
-             "uses training-aware neural architecture search and scaling, to jointly optimize "
-             "training speed and parameter efficiency. To optimize training, EfficientNetV2 "
-             "uses a combination of more commonly used depthwise convolutional 3 x 3 and "
-             "1 x 1 layers with fused convolutional layers (Tan & Le, 2021).")
+    with st.expander("Click to see EfficientNetV2."):
 
-    images = ['efficientnet_training_times', 'efficientnet_fused_layers']
-    titles = ['Training Time Comparisons', 'Standard vs. Fused Layers']
+        st.write("EfficientNetV2 was designed to address the probelm that comes with training "
+                "increasingly large datasets. Namely, the amount of training required to "
+                "achieve a converged model systematically tends to increase as there are more "
+                "parameters and data to be trained on. To overcome these problems, EfficientNetV2 "
+                "uses training-aware neural architecture search and scaling, to jointly optimize "
+                "training speed and parameter efficiency. To optimize training, EfficientNetV2 "
+                "uses a combination of more commonly used depthwise convolutional 3 x 3 and "
+                "1 x 1 layers with fused convolutional layers (Tan & Le, 2021).")
 
-    fig, ax = plt.subplots(1, 2)
+        images = ['efficientnet_training_times', 'efficientnet_fused_layers']
+        titles = ['Training Time Comparisons', 'Standard vs. Fused Layers']
 
-    for index, axes in enumerate(ax):
-        img = Image.open(f"images/{images[index]}.png")
-        axes.imshow(img)
-        axes.axis("off")
-        axes.set_title(f"{titles[index]}")
+        fig, ax = plt.subplots(1, 2)
 
-    with st.expander("Click to see EfficientNetV2 training time comparisons and fused layers"):
-        st.pyplot(fig, use_container_width = True)
+        for index, axes in enumerate(ax):
+            img = Image.open(f"images/{images[index]}.png")
+            axes.imshow(img)
+            axes.axis("off")
+            axes.set_title(f"{titles[index]}")
 
-    st.markdown("<h3>EfficientNetV2 Results</h3>", unsafe_allow_html = True)
+        with st.expander("Click to see EfficientNetV2 training time comparisons and fused layers"):
+            st.pyplot(fig, use_container_width = True)
 
-    st.write("During the training process, we unfroze the lower layers (Block 6.0) of the model "
-             "in order to maintain the feature extraction trained on the initial layers and focus "
-             "computational power on adjusting the weights of the final layers to achieve "
-             "better task specific classification. The model included 10,737,731 parameters, of "
-             "which, 3,918,613 were trainable. We initiated training with an Adam optimizer "
-             "set with a learning rate of 1e-4 and weight decay of 1e-4. We used Focal Loss "
-             "as the criterion because of the inbalanced nature of the dataset. During training "
-             "we slowed the learning rate to 1e-5 after 10 epochs. The change in learning rate "
-             "is marked on the plot below with a dotted line.")
+        st.markdown("<h3>EfficientNetV2 Results</h3>", unsafe_allow_html = True)
 
-    with st.expander("Click to see the trainging and validation metrics."):
-        img = Image.open("images/efficientnet_results.png")
-        st.image(img, use_container_width = False)
+        st.write("During the training process, we unfroze the lower layers (Block 6.0) of the model "
+                "in order to maintain the feature extraction trained on the initial layers and focus "
+                "computational power on adjusting the weights of the final layers to achieve "
+                "better task specific classification. The model included 10,737,731 parameters, of "
+                "which, 3,918,613 were trainable. We initiated training with an Adam optimizer "
+                "set with a learning rate of 1e-4 and weight decay of 1e-4. We used Focal Loss "
+                "as the criterion because of the inbalanced nature of the dataset. During training "
+                "we slowed the learning rate to 1e-5 after 10 epochs. The change in learning rate "
+                "is marked on the plot below with a dotted line.")
 
-    st.write("During the training phase, we were able to achieve a weighted F1-Score "
-             "for the validation set of .60. We then evaluated the model on a final "
-             "test set and achieved a weighted F1-Score of .60. The full classification "
-             "report is below.")
+        with st.expander("Click to see the trainging and validation metrics."):
+            img = Image.open("images/efficientnet_results.png")
+            st.image(img, use_container_width = False)
 
-    with st.expander("Classification results for the EfficientNetV2 model"):
-        img = Image.open("images/classification_report_efficientnet.png")
-        st.image(img, use_container_width = False)
+        st.write("During the training phase, we were able to achieve a weighted F1-Score "
+                "for the validation set of .60. We then evaluated the model on a final "
+                "test set and achieved a weighted F1-Score of .60. The full classification "
+                "report is below.")
 
-    st.write("We also extracted features for a subset of the images to better "
-             "understand what the Custom ResNet50 model interpreted as important "
-             "features.")
+        with st.expander("Classification results for the EfficientNetV2 model"):
+            img = Image.open("images/classification_report_efficientnet.png")
+            st.image(img, use_container_width = False)
 
-    with st.expander("Click to see feature maps for randomly selected images"):
-             img = Image.open("images/efficientnet_gradcam.png")
-             st.image(img, use_container_width = False) 
+        st.write("We also extracted features for a subset of the images to better "
+                "understand what the Custom ResNet50 model interpreted as important "
+                "features.")
+
+        with st.expander("Click to see feature maps for randomly selected images"):
+                img = Image.open("images/efficientnet_gradcam.png")
+                st.image(img, use_container_width = False) 
 
     st.markdown("<h2>ViT</h2>", unsafe_allow_html = True)
 
-    st.write("We additionally trained a transformer, ViT (Dosovitskiy et al., 2021), as "
-             "part of our project development. Transformers are the standard for training "
-             "and developing large language models, but until recently have not been used "
-             "with machine vision. To achive this, preset 'patch sizes' of images are sent "
-             "through a transformer, where a learnable token 'classification' token is "
-             "added to each sequence.")
+    with st.expander("Click to see ViT."):
 
-    with st.expander("Click to see a diagram of an image transformer"):
-        img = Image.open("images/vit_architecture.png")
-        st.image(img, use_container_width = True)
-    
-    st.write("As with the other models, we unfroze the lower layers, blocks 10 and 11, "
-             "to adjust the weights to task specific features, while freezing the "
-             "initial layers that are designed to capture broader, task invariant features. "
-             "The model included 85,667,355 parameters, of which, 14,175,774 were trainable. "
-             "We used an Adam optimizer, with inital learning rate and weight decay "
-             "set to 1e-3 and 1e-4, respectively. As in the other models, we used "
-             "Focal Loss as the criterion.")
+        st.write("We additionally trained a transformer, ViT (Dosovitskiy et al., 2021), as "
+                "part of our project development. Transformers are the standard for training "
+                "and developing large language models, but until recently have not been used "
+                "with machine vision. To achive this, preset 'patch sizes' of images are sent "
+                "through a transformer, where a learnable token 'classification' token is "
+                "added to each sequence.")
 
-    st.markdown("<h3>ViT Results</h3>", unsafe_allow_html = True)
+        with st.expander("Click to see a diagram of an image transformer"):
+            img = Image.open("images/vit_architecture.png")
+            st.image(img, use_container_width = True)
+        
+        st.write("As with the other models, we unfroze the lower layers, blocks 10 and 11, "
+                "to adjust the weights to task specific features, while freezing the "
+                "initial layers that are designed to capture broader, task invariant features. "
+                "The model included 85,667,355 parameters, of which, 14,175,774 were trainable. "
+                "We used an Adam optimizer, with inital learning rate and weight decay "
+                "set to 1e-3 and 1e-4, respectively. As in the other models, we used "
+                "Focal Loss as the criterion.")
 
-    st.write("During the training phase, the learning rate was lowered to 1e-4 and 1e-5 at "
-             "Epoches 10 and 20, respectively. We were able to achieve a weighted F1-Score "
-             ".585 on the validation data. The final test set revealed a weighted F1-Score "
-             "of .59. The full results from the training process and the classification "
-             "report are below.")
+        st.markdown("<h3>ViT Results</h3>", unsafe_allow_html = True)
 
-    with st.expander("Click to see the training results for the ViT."):
-        img = Image.open("images/ViT_results.png")
-        st.image(img, use_container_width = False)
+        st.write("During the training phase, the learning rate was lowered to 1e-4 and 1e-5 at "
+                "Epoches 10 and 20, respectively. We were able to achieve a weighted F1-Score "
+                ".585 on the validation data. The final test set revealed a weighted F1-Score "
+                "of .59. The full results from the training process and the classification "
+                "report are below.")
 
-    with st.expander("Click to see the classification report for the ViT."):
-        img = Image.open("images/classification_report_ViT.png")
-        st.image(img, use_container_width = False)
+        with st.expander("Click to see the training results for the ViT."):
+            img = Image.open("images/ViT_results.png")
+            st.image(img, use_container_width = False)
 
-    st.write("We also extracted features for a subset of the images to better "
-             "understand what the Custom ResNet50 model interpreted as important "
-             "features.")
+        with st.expander("Click to see the classification report for the ViT."):
+            img = Image.open("images/classification_report_ViT.png")
+            st.image(img, use_container_width = False)
 
-    with st.expander("Click to see feature maps for randomly selected images"):
-             img = Image.open("images/ViT_gradcam.png")
-             st.image(img, use_container_width = False) 
+        st.write("We also extracted features for a subset of the images to better "
+                "understand what the Custom ResNet50 model interpreted as important "
+                "features.")
+
+        with st.expander("Click to see feature maps for randomly selected images"):
+                img = Image.open("images/ViT_gradcam.png")
+                st.image(img, use_container_width = False) 
 
 
 
